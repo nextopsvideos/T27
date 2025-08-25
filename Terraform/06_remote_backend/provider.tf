acc0d6a5eafa7932fb19f41d77d7fbecc41aa10b
Charan <charan@nextpos.in>
2025-08-25 09:08:52 +0700

    storage account key removed

--- Terraform/06_remote_backend/provider.tf
+++ Terraform/06_remote_backend/provider.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   backend "azurerm" {
-    access_key           = "rtLYd93Gx+8o77UtHuSV3o/PshLjNfXsn0HmTwE5FqsB1TJTyhY5ytW2Ov9byt4ZUtjWp6/VOhMy+AStZgFhTQ=="  
+    access_key           = "asfjkdjsakcjkadjakndasknkn"  
     storage_account_name = "nextopssat27"                                 
     container_name       = "tfstate"                                  
     key                  = "terraform.tfstate"                   
@@ -16,5 +16,5 @@ terraform {
 provider "azurerm" {
   resource_provider_registrations = "none" 
   features {}
-  subscription_id = "a355c32e-4a22-4b05-aab4-be236850fa6e"
+  subscription_id = "xxxxxx-xxxx-xxxx--xxxxxx-xxx"
 }
