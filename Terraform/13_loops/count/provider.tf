b98e497e06cdd795a737ad9ef0d9b758514d5779
Charan <charan@nextpos.in>
2025-08-28 07:35:38 +0530

    subid updated

--- Terraform/13_loops/count/provider.tf
+++ Terraform/13_loops/count/provider.tf
@@ -5,19 +5,11 @@ terraform {
       version = "=4.41.0"
     }
   }
-  backend "azurerm" {
-    access_key           = "dasfklkasdjfkjsdakjkdjsfkjdasnckdsnkfadsfjksdjf"  
-    storage_account_name = "sasasa"                                 
-    container_name       = "modules"                                  
-    key                  = "DEV/terraform.tfstate"                   
-  }
+  
 }
 
 provider "azurerm" {
   resource_provider_registrations = "none" 
   features {}
-  subscription_id = "a355c32e-4a22-4b05-aab4-be236850fa6e"
-  client_id       = "a355c32e-4a22-4b05-aab4-be236850fa6e"
-  client_secret   = "a355c32e-4a22-4b05-aab4-be236850fa6e"
-  tenant_id       = "a355c32e-4a22-4b05-aab4-be236850fa6e"  
+  subscription_id = "0000000-00000-0000-00000-0000000"  
 }
