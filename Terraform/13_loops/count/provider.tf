43edd033f18e5dda23dc1031c4cbe9af6a717335
Charan <charan@nextpos.in>
2025-08-28 07:29:56 +0530

    test for secret scanning

--- Terraform/13_loops/count/provider.tf
+++ Terraform/13_loops/count/provider.tf
@@ -17,4 +17,7 @@ provider "azurerm" {
   resource_provider_registrations = "none" 
   features {}
   subscription_id = "a355c32e-4a22-4b05-aab4-be236850fa6e"
+  client_id       = "00000000-0000-0000-0000-000000000000"
+  client_secret   = "00000000-0000-0000-0000-000000000000"
+  tenant_id       = "10000000-0000-0000-0000-000000000000"  
 }
